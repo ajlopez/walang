7502d06b61589644bec7818b13a7db99f4527482
ajlopez <webmaster@ajlopez.com>
2019-01-13 11:40:40 -0300

    Binary operator node

--- test/nodes.js
+++ test/nodes.js
@@ -3,27 +3,41 @@ var n = require('../lib/nodes');
 var t = require('../lib/types');
 
 exports['integer constant node'] = function (test) {
-    var expr = n.constant(t.int, 10);
+    var node = n.constant(t.int, 10);
     
-    test.ok(expr);
-    test.equal(typeof expr, 'object');
-    test.equal(expr.value(), 10);
-    test.equal(expr.type(), t.int);
+    test.ok(node);
+    test.equal(typeof node, 'object');
+    test.equal(node.value(), 10);
+    test.equal(node.type(), t.int);
 };
 
 exports['name node'] = function (test) {
-    var expr = n.name('v');
+    var node = n.name('v');
     
-    test.ok(expr);
-    test.equal(typeof expr, 'object');
-    test.equal(expr.name(), 'v');
+    test.ok(node);
+    test.equal(typeof node, 'object');
+    test.equal(node.name(), 'v');
 };
 
 exports['variable node'] = function (test) {
-    var expr = n.variable(t.int, 'v');
+    var node = n.variable(t.int, 'v');
     
-    test.ok(expr);
-    test.equal(typeof expr, 'object');
-    test.equal(expr.name(), 'v');
-    test.equal(expr.type(), t.int);
+    test.ok(node);
+    test.equal(typeof node, 'object');
+    test.equal(node.name(), 'v');
+    test.equal(node.type(), t.int);
+};
+
+exports['binary operator node'] = function (test) {
+    var node = n.binary('+', n.constant(t.int, 40), n.constant(t.int, 2));
+    
+    test.ok(node);
+
+    test.equal(typeof node, 'object');
+    
+    test.equal(node.operator(), '+');
+    test.equal(node.left().type(), t.int);
+    test.equal(node.left().value(), 40);
+    test.equal(node.right().type(), t.int);
+    test.equal(node.right().value(), 2);
 };
