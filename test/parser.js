bfb26ab875de2c245d24c947dc9726ceec6ff293
ajlopez <webmaster@ajlopez.com>
2019-05-01 17:55:52 -0300

    Parse terms

--- test/parser.js
+++ test/parser.js
@@ -2,11 +2,24 @@
 const parser = require('../lib/parser');
 const geast = require('geast');
 
+function parse(test, type, text, expected) {
+    test.deepEqual(geast.toObject(parser.parse(type, text)), expected);
+}
+
 exports['parse constants'] = function (test) {
-    test.deepEqual(geast.toObject(parser.parse('integer', '42')), { ntype: 'constant', value: 42 });
-    test.deepEqual(geast.toObject(parser.parse('real', '3.14159')), { ntype: 'constant', value: 3.14159 });
-    test.deepEqual(geast.toObject(parser.parse('string', '"foo"')), { ntype: 'constant', value: 'foo' });
-    test.deepEqual(geast.toObject(parser.parse('boolean', 'true')), { ntype: 'constant', value: true });
-    test.deepEqual(geast.toObject(parser.parse('boolean', 'false')), { ntype: 'constant', value: false });
+    parse(test, 'integer', '42', { ntype: 'constant', value: 42 });
+    parse(test, 'real', '3.14159', { ntype: 'constant', value: 3.14159 });
+    parse(test, 'string', '"foo"', { ntype: 'constant', value: 'foo' });
+    parse(test, 'boolean', 'true', { ntype: 'constant', value: true });
+    parse(test, 'boolean', 'false', { ntype: 'constant', value: false });
+};
+
+exports['parse terms'] = function (test) {
+    parse(test, 'term', '42', { ntype: 'constant', value: 42 });
+    parse(test, 'term', '3.14159', { ntype: 'constant', value: 3.14159 });
+    parse(test, 'term', '"foo"', { ntype: 'constant', value: 'foo' });
+    parse(test, 'term', 'true', { ntype: 'constant', value: true });
+    parse(test, 'term', 'false', { ntype: 'constant', value: false });
+    parse(test, 'term', 'answer', { ntype: 'name', name: 'answer' });
 };
 
