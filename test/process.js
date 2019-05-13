38ebc871e17871c19c1c7109f5fa4b1178002e8b
ajlopez <webmaster@ajlopez.com>
2019-05-13 10:32:52 -0300

    Compiler process integer constant

---
+++ test/process.js
@@ -0,0 +1,13 @@
+
+const compilers = require('../lib/compilers');
+const geast = require('geast');
+
+exports['process integer constant'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const result = compiler.process(geast.constant(42));
+    
+    test.ok(result);
+    test.equal(result, 'i32.const 42');
+};
+
