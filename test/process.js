f882519d7306b043a768893ff2a8afe7e6b14034
ajlopez <webmaster@ajlopez.com>
2019-05-14 11:35:04 -0300

    Compiler process add integers

--- test/process.js
+++ test/process.js
@@ -11,3 +11,12 @@ exports['process integer constant'] = function (test) {
     test.equal(result, 'i32.const 42');
 };
 
+exports['process add integers'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const result = compiler.process(geast.binary('+', geast.constant(40), geast.constant(2)));
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'i32.add', [ 'i32.const 40' ], [ 'i32.const 2' ] ] );
+};
+
