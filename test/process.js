3c87e332fb81d1afea1cd0bd0c15bb88030b4e68
ajlopez <webmaster@ajlopez.com>
2019-05-15 09:38:24 -0300

    Process subtract integers

--- test/process.js
+++ test/process.js
@@ -20,3 +20,12 @@ exports['process add integers'] = function (test) {
     test.deepEqual(result, [ 'i32.add', [ 'i32.const 40' ], [ 'i32.const 2' ] ] );
 };
 
+exports['process subtract integers'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    const result = compiler.process(geast.binary('-', geast.constant(44), geast.constant(2)));
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'i32.sub', [ 'i32.const 44' ], [ 'i32.const 2' ] ] );
+};
+
