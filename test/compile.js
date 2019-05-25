031fe346c0d1c803ed57457a5b24e37b40be5ce2
ajlopez <webmaster@ajlopez.com>
2019-05-25 16:30:52 -0300

    Compile file

--- test/compile.js
+++ test/compile.js
@@ -1,6 +1,8 @@
 
 const walang = require('..');
 
+const path = require('path');
+
 exports['compile simple program'] = function (test) {
     const text = 'public void main() {}';
     
@@ -15,6 +17,16 @@ exports['compile add function program'] = function (test) {
     const text = 'public int add(int a, int b) { return a+b; }';
     
     const code = walang.compile(text);
+    
+    test.ok(code);
+    test.equal(typeof code, 'string');    
+    test.equal(code, '(func $add\r\n  (param $a i32)\r\n  (param $b i32)\r\n  (result i32)\r\n  (i32.add\r\n    (get_local $a)\r\n    (get_local $b)\r\n  )\r\n)\r\n');
+};
+
+exports['compile file add program'] = function (test) {
+    const filename = path.join(__dirname, 'files', 'add.wal');
+    
+    const code = walang.compileFile(filename);
     
     test.ok(code);
     test.equal(typeof code, 'string');    
