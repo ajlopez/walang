e95c737cebfe9b422c44e89a31a673f32b538e26
ajlopez <webmaster@ajlopez.com>
2019-05-28 13:19:30 -0300

    Process conditional with then and else

--- test/process.js
+++ test/process.js
@@ -243,6 +243,16 @@ exports['process conditional with then'] = function (test) {
     test.deepEqual(result, [ 'if', [ 'i32.const', 42 ], [ 'then', [ 'i32.const', 1 ] ] ]);
 };
 
+exports['process conditional with then and else'] = function (test) {
+    const compiler = compilers.compiler();
+    const node = geast.conditional(geast.constant(42, types.uint), geast.constant(1, types.uint), geast.constant(2, types.uint));
+    
+    const result = compiler.process(node);
+    
+    test.ok(result);
+    test.deepEqual(result, [ 'if', [ 'i32.const', 42 ], [ 'then', [ 'i32.const', 1 ] ], [ 'else', [ 'i32.const', 2 ] ] ]);
+};
+
 exports['process void function without arguments'] = function (test) {
     const compiler = compilers.compiler();
     const node = geast.function('foo', types.void, [], geast.sequence([ geast.constant(42) ]), visibilities.public);
