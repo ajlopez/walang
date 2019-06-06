655a449e57df486ebe8aba1e2f879fcddacc827d
ajlopez <webmaster@ajlopez.com>
2019-06-06 12:07:53 -0300

    Compile and run factorial program, WIP

--- test/compile.js
+++ test/compile.js
@@ -68,3 +68,25 @@ exports['compile program to binary and run add function'] = async function (test
     test.done();
 };
 
+exports['compile program to binary and run factorial function'] = async function (test) {
+    test.async();
+    
+    const filename = path.join(__dirname, 'files', 'factorial.wal');
+    const code = walang.compileFile(filename);
+
+    const wasmModule = wabt.parseWat('factorial.wal', code);
+    
+    test.ok(wasmModule);
+    
+    const { buffer } = wasmModule.toBinary({});
+    
+    const module = await WebAssembly.compile(new Buffer(buffer));
+    const instance = await WebAssembly.instantiate(module);
+    
+    //const result = instance.exports.factorial(3);
+    
+    //test.equal(result, 6);
+    
+    test.done();
+};
+
